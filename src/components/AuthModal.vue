049f5b3c59a83525f5d901a7171eda58212d8e8a
Тимофей <121519813+tima999222@users.noreply.github.com>
2023-11-13 13:04:36 +0000

    Added Auth component

---
+++ src/components/AuthModal.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="auth-modal">
+      <div class="auth-modal-content">
+        <!-- Здесь разместите содержимое вашего окна авторизации -->
+        <!-- Например, поля ввода логина и пароля, кнопка входа и т.д. -->
+        <button class="close-button" @click="closeModal">&times;</button>
+        <h2>Авторизация</h2>
+        <input type="text" placeholder="Логин" v-model="username" />
+        <input type="password" placeholder="Пароль" v-model="password" />
+        <button @click="login">Войти</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      login() {
+        // Здесь можно добавить логику для обработки входа
+        // Например, отправка данных на сервер для аутентификации
+        // В данном примере просто закрываем модальное окно
+        this.$emit('close');
+      },
+      closeModal() {
+        this.$emit('close');
+        }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .auth-modal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* Полупрозрачный фон */
+  }
+  
+  .auth-modal-content {
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+
+  .auth-modal input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 30px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.auth-modal button {
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.auth-modal button:hover {
+  background-color: #2980b9;
+}
+  </style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,9 +1,41 @@
 <template>
     <header>
-      <h1>Eat Eaze Delivery</h1>
+      <div class="header-content">
+        <h1>Eat Eaze Delivery</h1>
+        <div class="footer-buttons">
+          <button class="auth-button" @click="showAuthModal">Авторизоваться</button>
+          <button class="cart-button">
+            <img src="https://www.svgrepo.com/show/34974/shopping-cart.svg" alt="Корзина" />
+          </button>
+        </div>
+      </div>
+      <auth-modal v-if="isAuthModalVisible" @close="hideAuthModal" />
     </header>
   </template>
     
+    <script>
+    import AuthModal from './AuthModal.vue';
+    
+    export default {
+      components: {
+        AuthModal
+      },
+      data() {
+        return {
+          isAuthModalVisible: false
+        };
+      },
+      methods: {
+        showAuthModal() {
+          this.isAuthModalVisible = true;
+        },
+        hideAuthModal() {
+          this.isAuthModalVisible = false;
+        }
+      }
+    };
+    </script>
+
   <style scoped>
 header {
 background-color: #3498db; /* Цвет фона */
@@ -13,11 +45,46 @@ text-align: center;
 box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Тень */
 }
 
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 h1 {
 margin: 0;
 font-family: 'Your Chosen Font', sans-serif; /* Замените 'Your Chosen Font' на выбранный вами шрифт */
 font-size: 32px;
 }
+
+.footer-buttons {
+  display: flex;
+}
+
+.auth-button,
+.cart-button {
+  font-family: 'Trebuchet MS';
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin-left: 10px;
+  padding: 5px 10px;
+  font-size: 18px;
+  color: #fff;
+  transition: color 0.3s ease-in-out;
+}
+
+.auth-button:hover {
+  color: #34dbcb;
+}
+.cart-button:hover {
+  transform: scale(1.05); /* Цвет при наведении */
+}
+
+.cart-button img {
+  width: 50px; /* Замените на размер вашей картинки корзины */
+}
+
 </style>
   
   
